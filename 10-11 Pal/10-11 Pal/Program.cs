396069e5ed8f43d5f924d334183439cceb1844ad
AresMonster <aresfaeshemuroot7@gmail.com>
2025-04-17 19:58:32 +0500

    10-11

---
+++ 10-11 Pal/10-11 Pal/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _10_11_Pal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите r(полярное расстояние): ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите q(полярный угол): ");
+            double q = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Perevod(r, q));
+            (double x, double y) Perevod(double r, double q)
+            {
+                double _x = r * Math.Cos(q);
+                double _y = r * Math.Sin(q);
+                return (_x, _y);
+            }
+        }
+    }
+}
