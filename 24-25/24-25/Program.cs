aeeddb413fa81db30fdf81d2fb440106293c2674
AresMonster <aresfaeshemuroot7@gmail.com>
2025-05-02 16:59:11 +0500

    24-25

---
+++ 24-25/24-25/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace _24_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Введите количество элементов массива:");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+
+            Console.WriteLine("Введите элементы массива(ненулевые):");
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                if (array[i] * array[i - 1] >= 0)
+                {
+                    Console.WriteLine("Элемент черт: ");
+                    Console.WriteLine(i + 1);
+                    return;
+                }
+            }
+            Console.WriteLine(0);
+        }
+    }
+}
