77f1c5d1b0354305a929ecf3b132a795a7e13ef6
AresMonster <aresfaeshemuroot7@gmail.com>
2025-05-02 16:40:48 +0500

    16-17

---
+++ 16-17 Pal/16-17 Pal/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _16_17_Pal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во чисел a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int count = 1;
+            int result = 1;
+            while (count <= a)
+            {
+                count++;
+                result += count;
+            }
+            result = (result / a) - 1;
+            Console.WriteLine($"Среднее арифметическое чисел от 1 до {a}: {result}");
+        }
+    }
+}
