9f76205a3c8014bebcb15cc5eb2c054e394db786
AresMonster <aresfaeshemuroot7@gmail.com>
2025-05-02 17:09:08 +0500

    34-35

---
+++ 34-35 Pal/34-35 Pal/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _34_35_Pal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintOddNumbers();
+        }
+        static void PrintOddNumbers()
+        {
+            int number;
+            while (true)
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number == 0)
+                    break;
+                if (number % 2 != 0)
+                {
+                    Console.WriteLine("Нечетное число: " + number);
+                }
+            }
+        }
+    }
+}
