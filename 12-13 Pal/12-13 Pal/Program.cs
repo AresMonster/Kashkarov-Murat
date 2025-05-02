60f4f69ad8557f8395591d0004b879f123f5068f
AresMonster <aresfaeshemuroot7@gmail.com>
2025-05-02 16:38:55 +0500

    12-13

---
+++ 12-13 Pal/12-13 Pal/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _12_13_Pal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Первую точку: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите Вторую точку: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a == 0 || b == 0)
+            {
+                Console.WriteLine("Одна из точек лежит в начале системы координат");
+            }
+            else
+            {
+                Console.WriteLine("Ни одна из точек не лежит в начале системы координат");
+            }
+        }
+    }
+}
